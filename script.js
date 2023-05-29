43f9f770e32df3aacdab50293678837410a1b7c8
BurakBozan <burakbozantr@gmail.com>
2023-05-29 12:55:39 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let myLibrary = [];
+
+function Book(title) {
+  // the constructor...
+  this.title = title
+
+}
+
+const title = "hobbit";
+  
+
+function addBookToLibrary() {
+  // do stuff here
+  
+  
+  myLibrary.push(newBook);
+
+}
+
+const newBook = new newBook("hobbit");
